7a4103972d3f8793634806143eceef42a27237e0
n0vay <koshantanu@gmail.com>
2024-04-29 20:20:21 -0400

    added hook for restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { MENU_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-  const [menuItems, setMenuItems] = useState([]);
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_URL + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-    setMenuItems(
-      json.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card
-        .itemCards
-    );
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) {
     return <Shimmer />;
@@ -28,6 +13,8 @@ const RestaurantMenu = () => {
 
   const { name, cuisines, costForTwoMessage } = resInfo.cards[2].card.card.info;
 
+  const { itemCards } =
+    resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
   return (
     <div className="menu">
       <h1>{name}</h1>
@@ -36,7 +23,7 @@ const RestaurantMenu = () => {
       </h2>
       <h2>Menu</h2>
       <ul>
-        {menuItems.map((item) => (
+        {itemCards.map((item) => (
           <li key={item.card.info.id}>
             {" "}
             {item.card.info.name} - Rs.{item.card.info.price / 100}
