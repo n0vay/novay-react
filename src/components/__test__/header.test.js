c1097a31f2a59ff1bb2bb959af741e4920beebab
n0vay <koshantanu@gmail.com>
2024-05-23 15:59:14 -0400

    added test cases for header component

---
+++ src/components/__test__/header.test.js
@@ -0,0 +1,56 @@
+import { screen, render, fireEvent } from "@testing-library/react";
+import { Provider } from "react-redux";
+import appStore from "../../redux/appStore";
+import Header from "../Header";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("Should load Header Component with login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render cart", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const cart = screen.getByText(/Cart/);
+  expect(cart).toBeInTheDocument();
+});
+
+it("Should render cart with zero items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const cartItems = screen.getByText("Cart - (0) Items");
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("Should change Login button to Logout onClick", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(loginButton);
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+  expect(logoutButton).toBeInTheDocument();
+});
