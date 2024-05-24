b94ce2fa1bf2e9a759cc9020b7a217f3e19c855b
n0vay <koshantanu@gmail.com>
2024-05-24 14:59:30 -0400

    finalized cart test case

--- src/components/__test__/cart.test.js
+++ src/components/__test__/cart.test.js
@@ -1,15 +1,48 @@
-import { act, render } from "@testing-library/react";
-import MOCK_DATA from "../mocks/resMenuMock.json";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import MOCK_DATA_NAME from "../mocks/resMenuMock.json";
 import RestaurantMenu from "../RestaurantMenu";
+import { Provider } from "react-redux";
+import appStore from "../../redux/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
-    json: () => {
-      Promise.resolve(MOCK_DATA);
-    },
+    json: () => Promise.resolve(MOCK_DATA_NAME),
   });
 });
 
-it("Should load Restaurant Menu Component", async () => {
-  await act(async () => render(<RestaurantMenu />));
+it("Should load Restaurant Menu Components xD", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <RestaurantMenu />
+        </Provider>
+      </BrowserRouter>
+    )
+  );
+
+  const accordianheader = screen.getByText("Veg Pizza (14)");
+
+  fireEvent.click(accordianheader);
+
+  const addBtn = screen.getAllByRole("button", { name: "Add +" });
+
+  fireEvent.click(addBtn[0]);
+});
+
+it("should load Restauarant Menu component", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Provider store={appStore}>
+          <RestaurantMenu />
+        </Provider>
+      </BrowserRouter>
+    )
+  );
+
+  const accordionHeader = screen.getByText("Veg Pizza (14)");
+  fireEvent.click(accordionHeader);
+  expect(screen.getAllByTestId("foodItems").length);
 });
